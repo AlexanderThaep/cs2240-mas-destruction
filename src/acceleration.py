685b65cedb399e880ad58370bb7494b0954fcb69
AlexanderThaep <odomsathsia_tep@brown.edu>
2026-05-05 19:02:28 -0400

    tweak: print a bit of detail about gpu

--- src/acceleration.py
+++ src/acceleration.py
@@ -1,14 +1,13 @@
 import torch
 
-if torch.cuda.is_available():
-    print(f"GPU Available: {torch.cuda.get_device_name(0)}")
-else:
-    print("No GPU detected, running on CPU.")
-
 def get_device():
     gpu = "cpu"
     if torch.cuda.is_available():
+        print(f"GPU Available: {torch.cuda.get_device_name(0)}")
         gpu = "cuda"
     elif torch.mps.is_available():
+        print(f"GPU Available: {torch.mps.get_device_name(0)}")
         gpu = "mps"
+    else:
+        print("No GPU detected, running on CPU.")
     return torch.device(gpu)
